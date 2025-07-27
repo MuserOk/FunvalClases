25595f7d32a1d8dbdd1937f67fdcb11da75485d9
Mario <marioc720@gmail.com>
2025-07-27 02:23:38 -0300

    trabajando en el portafolio

---
+++ Portfolio/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.md\\:hidden').addEventListener('click', function() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('hidden');
+});
---
+++ misPracticas/main.js
@@ -0,0 +1,57 @@
+/*
+<div class="relative h-32 bg-gray-300">
+      <div
+        class="absolute botton-0 right-0 gb-blank text-white p-2 bg-gray-700"
+      >
+        Div Flotante
+      </div>
+      <div class="sticky top-0 bg-gray-400 opacity-35">
+        <strong>practi</strong>cando
+      </div>
+    </div>
+
+    <div class="flex flex-col md:flex-row gap-4 border-8">
+      <!-- Sidebar (Grid dentro de Flex) -->
+      <div class="grid grid-cols-2 gap-2 bg-blue-100 p-4 md:w-1/4">
+        <div class="bg-blue-400 p-2">Item 1</div>
+        <div class="bg-blue-400 p-2">Item 2</div>
+        <div class="bg-blue-400 p-2">Item 3</div>
+      </div>
+      <!-- Contenido principal con posicionamiento -->
+      <div class="relative bg-red-100 p-4 md:w-3/4">
+        <div class="absolute top-0 right-0 bg-red-500 text-white p-2">
+          ¡Nuevo!
+        </div>
+        <p>Contenido principal aquí...</p>
+      </div>
+    </div>
+
+    <div
+      class="max-w-md flex flex-wrap-reverse gap-6 justify-center bg-gray-500 m-20 border-amber-600 border-8 p-20"
+    >
+      <header class="bg-[url(../imagenes/dragon-ball-z.jpg)] border-2 p-8">
+        Header
+      </header>
+      <main class="bg-amber-950 border-8 p-5">Content</main>
+      <footer class="bg-blue-500 p-2">Footer</footer>
+      <img
+        src="../imagenes/dragon-ball-z.jpg"
+        alt="dragon ball z"
+        class="w-full object-cover shadow-2xl shadow-black rounded-2xl"
+      />
+      <p class="bg-pink-600">jkjnkdnkskfdsfndskfnk</p>
+    </div>
+
+    <div
+      class="grid grid-cols-3 gap-4 border-4 border-blue-900 m-2 bg-green-500 p-10"
+    >
+      <img
+        src="../imagenes/SHEMLONG.png"
+        alt="SHEMLONG"
+        class="border-4 border-orange-400 p-2"
+      />
+      <p class="p-5 m-8 border-2 border-blue-400">Parrafo 1</p>
+      <h2 class="bg-amber-300 p-10">Titulo h2</h2>
+      <img src="../imagenes/SSRoj.png" alt="" />
+    </div>
+*/
